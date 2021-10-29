813554517dc062a605078c03afa31b2fc9c10a0b
codepuncher <lee.hanbury@gmail.com>
2021-10-29 02:12:37 +0100

    feat: refresh nvim-tree on `BufWritePost`

--- nvim/lua/custom/nvim-tree.lua
+++ nvim/lua/custom/nvim-tree.lua
@@ -6,8 +6,10 @@ require'nvim-tree'.setup {
   },
 }
 
-local view = require'nvim-tree.view'
+-- Refresh NvimTree on file save
+vim.api.nvim_command [[autocmd BufWritePost lua require'nvim-tree'.refresh()]]
 
+local view = require'nvim-tree.view'
 local tree = {}
 tree.toggle = function()
   if view.win_open() then
