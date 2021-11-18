033ca486f7b752c1ea7b863fb9eb7e6a5d804365
codepuncher <lee.hanbury@gmail.com>
2021-11-18 07:48:04 +0000

    feat(nvim): add format command to eslint

--- nvim/lua/lsp/efm/eslint.lua
+++ nvim/lua/lsp/efm/eslint.lua
@@ -1,11 +1,12 @@
 return {
-  -- lintCommand = "eslint_d -f unix --stdin",
-  lintCommand = "eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}",
+  lintCommand = 'eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}',
   lintIgnoreExitCode = true,
   lintStdin = true,
   lintFormats = {
-    "%f(%l,%c): %tarning %m",
-    "%f(%l,%c): %rror %m",
+    '%f(%l,%c): %tarning %m',
+    '%f(%l,%c): %rror %m',
   },
   lintSource = "eslint",
+  formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
+  formatStdin = true
 }
