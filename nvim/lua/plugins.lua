949efea3312a961d0b855534c6d23e4d7c785749
codepuncher <lee.hanbury@gmail.com>
2021-11-24 10:23:13 +0000

    feat(nvim): telescope-fzf-native

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,6 +9,7 @@ return require('packer').startup(function(use)
       'nvim-telescope/telescope.nvim',
       requires = {
         'nvim-lua/plenary.nvim',
+        use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
       },
       config = function () require('custom.telescope') end,
     },
