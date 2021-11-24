36deee40c705f5217c4c57ce38a1c0578d26df1e
codepuncher <lee.hanbury@gmail.com>
2021-11-24 21:46:24 +0000

    move lua format changes

--- nvim/lua/lsp/efm/lua-format.lua
+++ nvim/lua/lsp/efm/lua-format.lua
@@ -1,4 +1,4 @@
 return {
-    formatCommand = 'lua-format -i --no-keep-simple-control-block-one-line --no-keep-simple-function-one-line --no-break-after-operator --break-after-table-lb --column-limit=150',
+    formatCommand = 'lua-format -i --no-keep-simple-control-block-one-line --no-keep-simple-function-one-line --no-break-after-operator --break-after-table-lb --double-quote-to-single-quote --column-limit=150',
     formatStdin = true
 }
