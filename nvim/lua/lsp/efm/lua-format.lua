3c6e88659e7db3df238351e5b7b07f2802769e46
codepuncher <lee.hanbury@gmail.com>
2021-11-24 21:31:13 +0000

    not sure if i like this formatting stuff

--- nvim/lua/lsp/efm/lua-format.lua
+++ nvim/lua/lsp/efm/lua-format.lua
@@ -1,4 +1,4 @@
 return {
-    formatCommand = 'lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=150 --break-after-table-lb',
+    formatCommand = 'lua-format -i --no-keep-simple-control-block-one-line --no-keep-simple-function-one-line --no-break-after-operator --break-after-table-lb --column-limit=150',
     formatStdin = true
 }
