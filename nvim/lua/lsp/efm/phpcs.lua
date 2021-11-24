92f9cfc9afc2f5aa841656bc0e411023f6d91d79
codepuncher <lee.hanbury@gmail.com>
2021-11-24 22:09:28 +0000

    style(nvim): format phpcs config

--- nvim/lua/lsp/efm/phpcs.lua
+++ nvim/lua/lsp/efm/phpcs.lua
@@ -1,12 +1,12 @@
 return {
-  lintCommand = [[$([ -n "$(command -v vendor/bin/phpcs)" ] && echo "vendor/bin/phpcs" || echo "phpcs") --standard=PSR12 --report=emacs -s - ${INPUT}]],
-  lintIgnoreExitCode = true,
-  lintStdin = true,
-  -- lintFormats = {
-  --   '%f(%l,%c): %tarning %m',
-  --   '%f(%l,%c): %rror %m',
-  -- },
-  lintSource = "phpcs",
-  -- formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
-  -- formatStdin = true,
+    lintCommand = [[$([ -n "$(command -v vendor/bin/phpcs)" ] && echo 'vendor/bin/phpcs' || echo 'phpcs') --standard=PSR12 --report=emacs -s - ${INPUT}]],
+    lintIgnoreExitCode = true,
+    lintStdin = true,
+    -- lintFormats = {
+    --   '%f(%l,%c): %tarning %m',
+    --   '%f(%l,%c): %rror %m',
+    -- },
+    lintSource = 'phpcs'
+    -- formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
+    -- formatStdin = true,
 }
