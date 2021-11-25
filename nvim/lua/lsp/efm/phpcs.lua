02f17c5f26a0a144fdc846d6ace7b268aae4afab
codepuncher <lee.hanbury@gmail.com>
2021-11-25 11:19:46 +0000

    fix(nvim): working lint formats for phpcs and use phpcs.xml if exists

--- nvim/lua/lsp/efm/phpcs.lua
+++ nvim/lua/lsp/efm/phpcs.lua
@@ -1,12 +1,12 @@
 return {
     lintSource = 'phpcs',
-    lintCommand = [[$([ -n "$(command -v vendor/bin/phpcs)" ] && echo 'vendor/bin/phpcs' || echo 'phpcs') --standard=PSR12 --report=emacs -s - ${INPUT}]],
+    lintCommand = [[$([ -n "$(command -v vendor/bin/phpcs)" ] && echo 'vendor/bin/phpcs' || echo 'phpcs') $([ ! -f phpcs.xml] && echo '--standard=PSR12') --report=emacs -s - ${INPUT}]],
     lintIgnoreExitCode = true,
     lintStdin = true,
-    -- lintFormats = {
-    --   '%f(%l,%c): %tarning %m',
-    --   '%f(%l,%c): %rror %m',
-    -- },
+    lintFormats = {
+        '%f:%l:%c: %rror - %m',
+        '%f:%l:%c: %tarning - %m'
+    },
     -- formatCommand = [[$([ -n "$(command -v vendor/bin/phpcbf)" ] && echo 'vendor/bin/phpcbf' || echo 'phpcbf') --standard=PSR12 - ${INPUT}]],
     formatSource = 'phpcbf',
     formatCommand = 'phpcbf --standard=PSR12 -q --stdin-path=${INPUT} -', -- This should work but doesn't...
