41c09de41ef0424a82275ec987fd8256d01a726e
codepuncher <lee.hanbury@gmail.com>
2021-11-28 00:40:28 +0000

    fix(nvim): phpcs lint command wrong standard

--- nvim/lua/lsp/efm/phpcs.lua
+++ nvim/lua/lsp/efm/phpcs.lua
@@ -1,12 +1,9 @@
 return {
     lintSource = 'phpcs',
-    lintCommand = [[$([ -n "$(command -v vendor/bin/phpcs)" ] && echo 'vendor/bin/phpcs' || echo 'phpcs') $([ ! -f phpcs.xml] && echo '--standard=PSR12') --report=emacs -s - ${INPUT}]],
+    lintCommand = [[$([ -n "$(command -v vendor/bin/phpcs)" ] && echo 'vendor/bin/phpcs' || echo 'phpcs') --report=emacs $([ ! -f phpcs.xml ] && echo '--standard=PSR12') --stdin-path=${INPUT} -q -s -]],
     lintIgnoreExitCode = true,
     lintStdin = true,
-    lintFormats = {
-        '%f:%l:%c: %rror - %m',
-        '%f:%l:%c: %tarning - %m'
-    },
+    lintFormats = {'%f:%l:%c: %rror - %m', '%f:%l:%c: %tarning - %m'},
     -- formatCommand = [[$([ -n "$(command -v vendor/bin/phpcbf)" ] && echo 'vendor/bin/phpcbf' || echo 'phpcbf') --standard=PSR12 - ${INPUT}]],
     formatSource = 'phpcbf',
     formatCommand = 'phpcbf --standard=PSR12 -q --stdin-path=${INPUT} -', -- This should work but doesn't...
