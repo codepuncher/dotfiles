970019d35499e74bd047099efa363e5007d1f57a
codepuncher <lee.hanbury@gmail.com>
2021-12-19 19:19:03 +0000

    feat: use tab for completion and snippet expansion/jumping

--- nvim/lua/custom/cmp.lua
+++ nvim/lua/custom/cmp.lua
@@ -1,68 +1,59 @@
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
-local function tab(fallback)
-  if cmp.visible() then
-    cmp.select_next_item()
-  else
-    fallback()
-  end
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
-local function shift_tab(fallback)
-  if cmp.visible() then
-    cmp.select_prev_item()
-  else
-    fallback()
-  end
+local feedkey = function(key, mode)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+local function tab(fallback)
+    if cmp.visible() then
+        cmp.select_next_item()
+    elseif vim.fn['vsnip#available'](1) == 1 then
+        feedkey('<Plug>(vsnip-expand-or-jump)', '')
+    elseif has_words_before() then
+        cmp.select_next_item()
+    else
+        fallback()
+    end
+end
+
+local function shift_tab()
+    if cmp.visible() then
+        cmp.select_prev_item()
+    elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+        feedkey('<Plug>(vsnip-jump-prev)', '')
+    end
 end
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-    end,
-  },
-  mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = cmp.mapping(tab, { 'c', 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(shift_tab, { 'c', 'i', 's' }),
-  },
-  sources = cmp.config.sources(
-    {
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' },
+    snippet = {
+        expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
+        end
     },
-    {
-      { name = 'buffer' },
-    }
-  ),
-  formatting = {
-    format = lspkind.cmp_format({
-      with_text = true,
-      maxwidth = 50,
-    }),
-  },
+    mapping = {
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ['<Tab>'] = cmp.mapping(tab, {'c', 'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(shift_tab, {'c', 'i', 's'})
+    },
+    sources = cmp.config.sources({{name = 'nvim_lsp'}, {name = 'vsnip'}}, {{name = 'buffer'}}),
+    formatting = {format = lspkind.cmp_format({with_text = true, maxwidth = 50})}
 })
 
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
-})
+cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
 
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
+cmp.setup.cmdline(':', {sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})})
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
