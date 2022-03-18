1008b3bb9451a98f452d8847d891d4224da339e7
codepuncher <lee.hanbury@gmail.com>
2022-03-18 18:10:33 +0000

    feat(nvim): new git keymaps

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -84,7 +84,9 @@ map('n', '<Leader>gs', '<Cmd>Git<CR>', noremap)
 map('n', '<Leader>gh', '<Cmd>diffget //3<CR>', noremap)
 map('n', '<Leader>gl', '<Cmd>diffget //2<CR>', noremap)
 map('n', '<Leader>gca', '<Cmd>Git commit -a<CR>', noremap)
-map('n', '<Leader>ga', "<Cmd>Git add % <bar> echo 'Staged ' . @%<CR>", noremap)
+map('n', '<Leader>gaf', "<Cmd>Git add % <bar> echo 'Staged ' . @%<CR>", noremap)
+map('n', '<Leader>gaa', "<Cmd>Git add . <bar> echo 'Staged tracked files'<CR>", noremap)
+map('n', '<Leader>gaA', "<Cmd>Git add -A <bar> echo 'Staged tracked and untracked files'<CR>", noremap)
 map('n', '<Leader>gp', '<Cmd>Git push<CR>', noremap)
 
 -- Symbols Outline
