991e3f8406af6fd7618146955245da48b605d24f
codepuncher <lee.hanbury@gmail.com>
2022-04-04 02:34:25 -0500

    cleanup

--- nvim/lua/custom/nvim-tree.lua
+++ nvim/lua/custom/nvim-tree.lua
@@ -1,11 +1,25 @@
 require('nvim-tree').setup({
   auto_close = true,
+  auto_reload_on_write = true,
   open_on_setup = false,
-  -- diagnostics = {enable = true},
-  update_focused_file = { enable = true, update_cwd = false, ignore_list = {} },
-  git = { ignore = false },
-  view = { auto_resize = true, side = 'left', width = 50 },
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+    ignore_list = {},
+  },
+  view = {
+    width = 50,
+    side = 'left',
+    mappings = {
+      list = {
+        {
+          key = '<C-e>',
+          action = '',
+        },
+      },
+    },
+  },
+  git = {
+    ignore = false,
+  },
 })
-
--- Refresh NvimTree on file save
-vim.api.nvim_command([[autocmd BufWritePost lua require'nvim-tree'.refresh()]])
