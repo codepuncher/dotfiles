d2c2cb03ba2e84efc73ebd4991f07d9f935a9730
codepuncher <lee.hanbury@gmail.com>
2022-07-13 16:25:48 +0100

    🐛(nvim): change `intelephense` root dir

--- nvim/lua/lsp/servers/intelephense.lua
+++ nvim/lua/lsp/servers/intelephense.lua
@@ -1,12 +1,24 @@
 local _M = {}
 
 _M.setup = function(on_attach, capabilities)
-  require('lspconfig').intelephense.setup({
+  local _lspconfig, lspconfig = pcall(require, 'lspconfig')
+  if not _lspconfig then
+    return
+  end
+
+  lspconfig.intelephense.setup({
     on_attach = on_attach,
     capabilities = capabilities,
     flags = {
       debounce_text_changes = 150,
     },
+    root_dir = function(pattern)
+      local cwd = vim.loop.cwd()
+      local root = lspconfig.util.root_pattern('composer.json', '.git', 'wp-config.php')(pattern)
+
+      -- prefer cwd if root is a descendant
+      return lspconfig.util.path.is_descendant(cwd, root) and cwd or root
+    end,
     init_options = {
       documentFormatting = false,
     },
