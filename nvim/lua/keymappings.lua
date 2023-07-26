30507c62db24b0e0298817b169e9ecfd642da3b1
codepuncher <lee.hanbury@gmail.com>
2023-07-26 11:29:26 +0100

    update comment

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -137,7 +137,7 @@ wk.register({
     },
     ['='] = {
       "<cmd>Git push -u origin @ | :execute '!gh pr create -f'<cr>",
-      '[GIT] Push new branch, create a PR and watch the Actions job',
+      '[GIT] Push new branch, create a PR',
     },
     mpr = { '<cmd>!gh pr merge -md<cr>', '[GIT] Merge PR and delete branch' },
     vpr = { '<cmd>!gh pr view -w<cr>', '[GIT] View PR in browser' },
