36f1b50213ff4567e805354c1b8018de0a60f89c
codepuncher <lee.hanbury@gmail.com>
2024-01-30 10:31:30 +0000

    new layout for tmux dev

--- tmux/layouts/dev.sh
+++ tmux/layouts/dev.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-THEME_DIR=$(fd '(composer|package).json' {wp-content,web/app}/themes 2>/dev/null | head -n 1 | xargs dirname || '#{pane_current_path}')
+THEME_DIR="$(fd '(composer|package).json' {wp-content,web/app}/themes 2>/dev/null | head -n 1 | xargs dirname)"
+if [[ -z "${THEME_DIR}" ]]; then
+	THEME_DIR='#{pane_current_path}'
+fi
+
+TRELLIS_DIR="${PWD}/../trellis/"
+PANE_THIRD_WORKING_DIRECTORY=''
+if [[ -d "${TRELLIS_DIR}" ]]; then
+	PANE_SECOND_WORKING_DIRECTORY="${TRELLIS_DIR}"
+	PANE_THIRD_WORKING_DIRECTORY="${THEME_DIR}"
+else
+	PANE_SECOND_WORKING_DIRECTORY="${THEME_DIR}"
+fi
+
 tmux \
-	split-window -v -p 25 -c '#{pane_current_path}' \; \
-	split-window -h -c "${THEME_DIR}" \; \
-	select-pane -L \; \
-	send-keys 'cd ../trellis || exit' Enter \; \
+	split-window -v -p 25 -c "${PANE_SECOND_WORKING_DIRECTORY}"
+
+if [[ -n "${PANE_THIRD_WORKING_DIRECTORY}" ]]; then
+	tmux \
+		split-window -h -c "${PANE_THIRD_WORKING_DIRECTORY}"
+fi
+
+tmux \
 	select-pane -U \; \
 	send-keys nvim
